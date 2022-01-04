3a31384ef2df4711b65e8e480ad6ca6b3df27d48
Technik Tüftler <technik.tueftler@gmail.com>
2022-01-04 22:42:33 +0100

    - update error-log path - no long sleep before database push. Depends now on timedelta

--- source/main.py
+++ source/main.py
@@ -1,12 +1,31 @@
 from bme68x import BME68X
-import bme68xConstants as cst
+import bme68xConstants as cnst
 import bsecConstants as bsec
-from time import sleep
+import time
+from datetime import datetime, timedelta
 import db
 
 print("Start sensor 1 - Keller Arbeitszimmer")
-bme = BME68X(cst.BME68X_I2C_ADDR_HIGH, 1)
-bme.set_sample_rate(bsec.BSEC_SAMPLE_RATE_LP)
+
+"""temp_prof = [50, 50, 350, 350, 350, 140, 140, 350, 350, 350]
+dur_prof = [9800, 9800, 140, 140, 19320, 9800, 9800, 140, 140, 19320]
+print("Start Kalibrierung mit Heizprofil HP-331 - Dauer ca. 78,4 Sekunden")
+# 567.774761, 272.294563
+start_time = datetime.datetime.now()
+sensor = BME68X(cnst.BME68X_I2C_ADDR_HIGH, 0)
+print(sensor.get_data())
+print(sensor.set_heatr_conf(cnst.BME68X_ENABLE, temp_prof, dur_prof, cnst.BME68X_SEQUENTIAL_MODE))
+print(sensor.get_data())
+print("Kalibrierung mit Heizprofil HP-331 abgeschlossen")
+end_time = datetime.datetime.now()
+print(f"Gesamtzeit für Kalibrierung: {(end_time-start_time).total_seconds()}")"""
+
+print("Start zyklische Messung")
+delta_push_to_database_time = timedelta(seconds=297)
+last_push_to_database_time = datetime.now()
+
+sensor = BME68X(cnst.BME68X_I2C_ADDR_HIGH, 0)
+sensor.set_sample_rate(bsec.BSEC_SAMPLE_RATE_LP)
 
 
 def get_data(sensor):
@@ -17,22 +36,25 @@ def get_data(sensor):
         print(e)
         return None
     if data is None or data == {}:
-        sleep(0.1)
-        print("Fehler 1")
+        time.sleep(0.1)
         return None
     else:
-        sleep(3)
+        time.sleep(3)
         return data
 
 
 try:
     while True:
-        bsec_data = get_data(bme)
+        bsec_data = get_data(sensor)
         while bsec_data is None:
-            bsec_data = get_data(bme)
+            bsec_data = get_data(sensor)
+        """print(f'Nr.: {bsec_data["sample_nr"]}'.center(140, "-"))
         print(bsec_data)
         db.add_new_measurement(1, **bsec_data)
-        sleep(897)
+        time.sleep(297)"""
+        if (datetime.now() - last_push_to_database_time) >= delta_push_to_database_time:
+            db.add_new_measurement(1, **bsec_data)
+            last_push_to_database_time = datetime.now()
 except KeyboardInterrupt:
     print("\nBeenden durch Tastatur")
 
